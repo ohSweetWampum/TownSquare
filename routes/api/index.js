e35058cc6c9782bb8688bafd095a04925edbff27
mtgibson8 <126990635+mtgibson8@users.noreply.github.com>
2023-05-24 09:52:43 -0500

    added even more comments, really too many

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,13 @@
-const router = require("express").Router();
-const userRoutes = require("./userRoutes");
-const thoughtRoutes = require("./thoughtRoutes");
+// Import necessary packages/modules
+const router = require("express").Router(); // Express Router to separate route handling
+const userRoutes = require("./userRoutes"); // Import routes for user related endpoints
+const thoughtRoutes = require("./thoughtRoutes"); // Import routes for thought related endpoints
 
+// When we visit /users, we use the routes set up in userRoutes
 router.use("/users", userRoutes);
+
+// When we visit /thoughts, we use the routes set up in thoughtRoutes
 router.use("/thoughts", thoughtRoutes);
 
+// Export the router to be used in other parts of the application
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
-const router = require('express').Router();
-const apiRoutes = require('./api');
+// Import necessary packages/modules
+const router = require("express").Router(); // Express Router to separate route handling
+const apiRoutes = require("./api"); // Importing api routes (that's where all routes are bundled together)
 
-router.use('/api', apiRoutes);
+// All api routes are prepended with '/api' to organise url paths
+router.use("/api", apiRoutes);
 
-router.use((req, res) => res.send('Wrong route!'));
+// Middleware for handling requests to any non-API routes
+// If no API routes are hit, this code block runs sending an error message
+router.use((req, res) => res.send("Wrong route!"));
 
+// Export the router to be used in other parts of the application
 module.exports = router;
